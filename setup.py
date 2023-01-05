ed9c425b7cbe81f636d9b3cfd4707334b2aaf146
rekumar <rekumar@lbl.gov>
2023-01-04 16:56:47 -0800

    setup.py update

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import List
 from setuptools import setup
 from setuptools import find_packages
 import os
@@ -16,17 +17,12 @@ with open(os.path.join(this_dir, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
-# with open('megnet/__init__.py', encoding='utf-8') as fd:
-#     try:
-#         lines = ''
-#         for item in fd.readlines():
-#             item = item
-#             lines += item + '\n'
-#     except Exception as exc:
-#         raise Exception('Caught exception {}'.format(exc))
-
-
-# version = re.search('__version__ = "(.*)"', lines).group(1)
+def parse_requirements(filename: str) -> List[str]:
+    """load requirements from a pip requirements file"""
+    filepath = Path(__file__).parent / filename
+    with open(filepath, "r") as f:
+        packages = [line.strip() for line in f if line and not line.startswith("#")]
+    return packages
 
 
 setup(
@@ -40,12 +36,8 @@ setup(
     download_url="https://github.com/rekumar/alab_data",
     license=__license__,
     python_requires=">=3.8",
-    install_requires=[
-        package.strip("\n")
-        for package in (Path(__file__).parent / "requirements.txt")
-        .open("r", encoding="utf-8")
-        .readlines()
-    ],
+    install_requires=parse_requirements("requirements.txt"),
+    extras_require={"dev": parse_requirements("requirements-dev.txt")},
     packages=find_packages(),
     include_package_data=True,
     keywords=[
