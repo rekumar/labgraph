24d44f502570fb6f7e114913df8d5d3850da5bc9
rekumar <rekumar@lbl.gov>
2023-02-23 15:23:16 -0800

    fix tests to make temporary config file

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -2,6 +2,34 @@ import pytest
 
 from .example_system import *
 from .example_data import *
+import os
+import toml
+import pymongo
+
+
+@pytest.fixture(autouse=True)
+def make_config_file():
+    config_path = os.path.abspath(os.path.join(os.curdir, "labgraph_test_config.toml"))
+
+    config = {
+        "mongodb": {
+            "host": "localhost",
+            "port": 27017,
+            "db_name": "Labgraph_Test",
+        }
+    }
+    with open(config_path, "w") as f:
+        toml.dump(config, f)
+    os.environ["LABGRAPH_CONFIG"] = config_path
+
+    yield
+
+    os.remove(config_path)
+    del os.environ["LABGRAPH_CONFIG"]
+    pymongo.MongoClient(
+        host=config["mongodb"]["host"],
+        port=config["mongodb"]["port"],
+    ).drop_database(config["mongodb"]["db_name"])
 
 
 @pytest.fixture
