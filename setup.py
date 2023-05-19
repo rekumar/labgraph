34d61b2999b3d49d0f9ba839ba4dd42f9e78b615
rekumar <rekumar@lbl.gov>
2023-05-19 13:45:02 -0700

    remove scipy, no longer required

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open(THIS_DIR / "README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 
-def read_requirements(filepath: Path) -> List[str]:
-    with open(filepath, encoding="utf-8") as fd:
-        return [
-            package.strip("\n")
-            for package in fd.readlines()
-            if not package.startswith("#")
-        ]
+# def read_requirements(filepath: Path) -> List[str]:
+#     with open(filepath, encoding="utf-8") as fd:
+#         return [
+#             package.strip("\n")
+#             for package in fd.readlines()
+#             if not package.startswith("#")
+#         ]
 
 
 def get_version(filepath: Path) -> str:
@@ -27,11 +27,11 @@ def get_version(filepath: Path) -> str:
 
 
 version = get_version(THIS_DIR / "labgraph" / "__init__.py")
-requirements = read_requirements(THIS_DIR / "requirements.txt")
-dev_requirements = read_requirements(THIS_DIR / "requirements-dev.txt")
+# requirements = read_requirements(THIS_DIR / "requirements.txt")
+# dev_requirements = read_requirements(THIS_DIR / "requirements-dev.txt")
 
 setup(
-    name="labgraph",
+    name="labgraph-db",
     version=version,
     description="A library for storing experimental materials science data as graphs using MongoDB.",
     long_description=long_description,
@@ -41,8 +41,26 @@ setup(
     download_url="https://github.com/rekumar/labgraph",
     license="MIT",
     python_requires=">=3.8",
-    install_requires=requirements,
-    extras_require={"dev": dev_requirements},
+    install_requires=[
+        "pymongo>=3.11.2",
+        "networkx>=2.6",
+        "numpy>=1.23.0",
+        "matplotlib>=3.5.2",
+        "toml",
+        "pydantic",
+    ],
+    extras_require={
+        "dev": [
+            "sphinx >= 3.2.1",
+            "sphinx_book_theme == 0.1.7",
+            "recommonmark ~= 0.7.1",
+            "sphinx-autodoc-typehints >= 1.12.0",
+            "pytest >= 6.2.5",
+            "pytest_reraise >= 2.1.1",
+            "pylint >= 2.11.1",
+            "pytest-env ~= 0.6.2",
+        ]
+    },
     packages=find_packages(),
     include_package_data=True,
     keywords=[
